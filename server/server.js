2ca6be9fc16810baf3dbed698119e8405c77ec79
DankangE <ekffy99@gmail.com>
2023-10-23 15:57:11 +0900

    server 생성 및 회원가입 테스트 post 작성

--- server/server.js
+++ server/server.js
@@ -1,18 +1,57 @@
-const express = require("express"); 
+const express = require("express");
+const session = require('express-session');
+const path = require('path');
 const app = express();
 const port = 3001; // react의 기본값은 3000이니까 3000이 아닌 아무 수
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mysql = require("mysql2"); // mysql 모듈 사용
 
-var connection = mysql.createConnection({
-    host : "localhost",
-    user : "root", //mysql의 id
-    password : "root", //mysql의 password
-    database : "tistory", //사용할 데이터베이스
-});
+const db = require('../lib/db');
+// const sessionOption = require('./lib/sessionOption');
+const bcrypt = require('bcrypt');
 
-connection.connect();
+// app.use(express.static(path.join(__dirname, '/public')));
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// app.use(cors());
+
+// var check;
+
+// const conn = mysql.createConnection({
+//     host : "localhost",
+//     user : "root", //mysql의 id
+//     password : "root", //mysql의 password
+//     database : "test", //사용할 데이터베이스
+// });
+
+
+// app.get('/api/tdata', function (req, res) {
+//     const sql = "select * from customers;"
+//     conn.query(sql, function (err, result, fields){
+//         res.send('Hello' + JSON.stringify(result));
+//     });
+// });
+
+// app.get('/', function(req, res) {
+//     res.send(JSON.stringify(check));
+// });
+
+// conn.connect(function(err) {
+//     if (err) throw err;
+//         console.log("Mysql DB Connected!");
+// });
+
+
+// var connection = mysql.createConnection({
+//     host : "localhost",
+//     user : "root", //mysql의 id
+//     password : "123456", //mysql의 password
+//     database : "test", //사용할 데이터베이스
+// });
+
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -22,10 +61,11 @@ app.get('/', (req, res) =>{
     res.send('test server start')
 })
 
-app.post("/idplz", (req,res)=>{
-    const test = req.body.test;
+app.post("/signup", (req,res)=>{
+    const body = req.body.body;
+    const head = req.body.head;
     // console.log(req.body);
-    connection.query("INSERT INTO test (test_body) values (?)",[test],
+    db.query("INSERT INTO test (test_body, test_head) values (?, ?)",[body, head],
     function(err,rows,fields){
         if(err){
             console.log("실패");
@@ -35,8 +75,6 @@ app.post("/idplz", (req,res)=>{
             // console.log(rows);
         };
     });
-
-    
 });
 
 app.listen(port, ()=>{
