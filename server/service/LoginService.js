e9a479e3a835ba9c8ecac315c09cbb0d3ab27082
DankangE <ekffy99@gmail.com>
2023-10-27 16:38:05 +0900

    [Feat] 로그인 DB 연동 : 로그인 성공시 메인화면 전환

--- server/service/LoginService.js
+++ server/service/LoginService.js
@@ -9,10 +9,10 @@ import * as userRepository from "../repository/LoginRepository.js";
 export const getUserLogin = (req, res) => {
   // 요청 request 에 있는 쿼리 파라미터(url)를 받는다.
   // 만약 나머지 메소드는 req.body 에서 데이터를 꺼낸다.
-  const user_no = req.query.user_no;
+  const {user_id, user_pw} = req.query;
   try {
     // repository에 요청을 전달한다.
-    userRepository.findByUserLogin(user_no).then((result) => {
+    userRepository.findByUserLogin(user_id, user_pw).then((result) => {
       // repository에서 반환된 결과를 controller에 반환한다.
       res.status(200).json(result);
     });
