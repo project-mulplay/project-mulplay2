be6f13fd7c0993c0d247f5c9a37ca150e9997d9c
jeong <otati3@gmail.com>
2023-10-27 14:44:52 +0900

    [fix]가이드 파일명 소문자로 변경

--- src/pages/guidepages/guidepage.js
+++ src/pages/guidepages/guidepage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Guidepage.css";
+import "./guidepage.css";
 import img1 from "../../assets/image/guide1.png";
 import img2 from "../../assets/image/guide2.png";
 import img3 from "../../assets/image/guide3.png";
