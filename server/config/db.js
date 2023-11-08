3b49e43bb1bfbde8c888c8d25c5eb6ca386ca280
DankangE <ekffy99@gmail.com>
2023-11-08 15:44:29 +0900

    DB 리콜

--- server/config/db.js
+++ server/config/db.js
@@ -1,16 +1,20 @@
 import mysql from "mysql2/promise";
-import dotenv from "dotenv";
-// import fs from "fs";
-// import yaml from "js-yaml";
+// import dotenv from "dotenv";
+import fs from "fs";
+import yaml from "js-yaml";
 
-// const config = yaml.load(fs.readFileSync("mulplay.yaml", "utf8"));
-dotenv.config();
+const config = yaml.load(fs.readFileSync("mulplay.yaml", "utf8"));
+// dotenv.config();
 
 const connection = mysql.createConnection({
-  host: process.env.DATABASE_HOST,
-  user: process.env.DATABASE_USERNAME, //mysql의 id
-  password: process.env.DATABASE_PASSWORD, //mysql의 password
-  database: process.env.DATABASE_NAME, //사용할 데이터베이스
+  // host: process.env.DATABASE_HOST,
+  // user: process.env.DATABASE_USERNAME, //mysql의 id
+  // password: process.env.DATABASE_PASSWORD, //mysql의 password
+  // database: process.env.DATABASE_NAME, //사용할 데이터베이스
+  host: config.url,
+  user: config.id, //mysql의 id
+  password: config.pw, //mysql의 password
+  database: "mulplay", //사용할 데이터베이스
   //   port: 3306,
 });
 
